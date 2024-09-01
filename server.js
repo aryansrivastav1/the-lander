3a795928510e2f494fda60f469b2c14b076b5fcf
Aryan Sapiens <166945698+aryansrivastav1@users.noreply.github.com>
2024-09-01 14:40:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.post("/submit-form", (req, res) => {
   formData
     .save()
     .then(() => {
-      res.status(200).send("<h1>Your details have been successfully submitted!</h1>");
+      res.status(200).send("<h1>Your details have been successfully submitted!</h1><br><br><p><a href="aryansrivastav1.github.io/the-lander/">Click for homepage</a><p>");
     })
     .catch((err) => {
       console.error("Error saving data:", err);
